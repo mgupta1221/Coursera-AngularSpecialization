01d183048e6d27b7d03df2a43198cd53f9513c39
Mohit Gupta <mohgupt@adobe.com>
2021-05-27 18:47:49 +0530

    Single Page Applications Part 2

--- Angular/conFusion/src/app/dish-detail/dish-detail.component.ts
+++ Angular/conFusion/src/app/dish-detail/dish-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DishService } from '../services/dish.service';
 import { Dish } from './../shared/dish'
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dish-detail',
@@ -7,10 +10,20 @@ import { Dish } from './../shared/dish'
   styleUrls: ['./dish-detail.component.scss']
 })
 export class DishDetailComponent implements OnInit {
-  @Input() dish: Dish;
-  constructor() { }
+  
+  dish: Dish;
+
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
