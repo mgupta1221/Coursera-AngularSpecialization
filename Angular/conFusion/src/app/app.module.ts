729bf3596b4dba57ee0aa1b1e88a81fdde17b8f3
Mohit Gupta <mohgupt@adobe.com>
2021-05-27 15:30:27 +0530

    Basic Service

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,7 +37,7 @@ import { DishDetailComponent } from './dish-detail/dish-detail.component';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angular/conFusion/src/app/menu/menu.component.ts
+++ Angular/conFusion/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DishService } from '../services/dish.service';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 
@@ -11,13 +12,14 @@ import { DISHES } from '../shared/dishes';
 
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[];
   selectedDish: Dish;
 
-  constructor() {
+  constructor(private dishService: DishService) {
   }
 
   ngOnInit(): void {
+    this.dishes = this.dishService.getDishes();
   }
   onSelect(dish: Dish) {
     this.selectedDish = dish;
